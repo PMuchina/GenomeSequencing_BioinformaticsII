a0a1dbb2583d2fb8aa10d525495b6bdb322c770a
Kalyani Asthana <kalyaniasthana@gmail.com>
2018-10-13 17:24:26 +0530

    string spelled by a genome path

--- w_1.py
+++ w_1.py
@@ -1,11 +1,25 @@
+def inputFile(file):
+	kmers= []
+	with open(file) as f:
+		for line in f:
+			line = line[: -1]
+			kmers.append(line)
+	return kmers
+
 def Composition(dna, k):
 	kmers = []
 	for i in range(0, len(dna) - k + 1):
 		kmers.append(dna[i:i+k])
 	return kmers
 
-dna = 'CAATCCAAC'
-k = 5
-kmers = Composition(dna, k)
-for kmer in kmers:
-	print kmer
+def StringSpelledByAGenomePath(kmers):
+	string = []
+	string.append(kmers[0])
+	x = len(kmers[0])
+	for i in range(1, len(kmers)):
+		string.append(kmers[i][x - 1])
+	string = ''.join(string)
+	return string
+
+kmers = inputFile('../Downloads/dataset_198_3.txt')
+print StringSpelledByAGenomePath(kmers)
